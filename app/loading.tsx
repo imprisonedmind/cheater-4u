541a46d146e0d558b8323215bcda5298ea7e7083
imprisonedmind <imahowiie@gmail.com>
2025-03-26 00:48:21 +0200

    add loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen pt-24 flex justify-center">
+      <div className="w-16 h-16 border-4 border-green-500 border-t-4 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
